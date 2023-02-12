dcb156cdac1992190f11cd11daa8c2f5684c5c22
mikhail.zavidov <mikezav68@gmail.com>
2023-02-12 17:43:16 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        String abc = "телефон нофелет";
+        boolean palindrome = true;
+        int i = 0;
+        int j = abc.length() - 1;
+        while (i < abc.length()) {
+            if (abc.charAt(i) != abc.charAt(j)) {
+                palindrome = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+        if (palindrome) {
+            System.out.println("Строка является палиндромом");
+        } else {
+            System.out.println("Строка не является палиндромом");
+        }
+    }
+}
